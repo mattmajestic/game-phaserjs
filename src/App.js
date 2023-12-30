cfbfe1af9804228d76e3b7fdd85b4bdbf94aba1e
Matt Majestic <33904170+mattmajestic@users.noreply.github.com>
2023-12-30 18:21:31 -0500

    add game

--- src/App.js
+++ src/App.js
@@ -1,25 +1,115 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
+import Phaser from 'phaser';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    useEffect(() => {
+        const config = {
+            type: Phaser.AUTO,
+            width: 800,
+            height: 600,
+            parent: 'phaser-game-container',
+            scene: {
+                preload: preload,
+                create: create,
+                update: update
+            }
+        };
+
+        let game = new Phaser.Game(config);
+        let startText, enterText, countdownText;
+        let countdown = 5;
+        let isCountingDown = false;
+        let pulsing = false;
+
+        function preload() {
+            this.load.image('homeScreen', 'background.png');
+        }
+
+        function create() {
+            this.add.image(400, 300, 'homeScreen');
+
+            const textStyle = {
+                fontSize: '40px',
+                fontFamily: 'Arial',
+                color: '#ffffff',
+                backgroundColor: '#000000',
+                padding: 20,
+                align: 'center'
+            };
+
+            const titleStyle = {
+              fontSize: '48px',
+              fontFamily: 'Arial',
+              color: '#ffffff',
+              align: 'center'
+          };
+      
+          this.add.text(this.cameras.main.centerX, 20, 'Majestic Coding Game', titleStyle).setOrigin(0.5, 0);
+      
+
+            startText = this.add.text(this.cameras.main.centerX, this.cameras.main.centerY / 2, 'Click to Start', textStyle);
+            startText.setOrigin(0.5, 0.5).setInteractive();
+
+            enterText = this.add.text(this.cameras.main.centerX, this.cameras.main.centerY / 2 + 60, 'Press Enter to Start', textStyle);
+            enterText.setOrigin(0.5, 0.5);
+
+            countdownText = this.add.text(this.cameras.main.centerX, this.cameras.main.centerY, '', textStyle);
+            countdownText.setOrigin(0.5, 0.5).setVisible(false);
+
+            startText.on('pointerdown', startCountdown);
+            enterText.on('pointerdown', startCountdown);
+            this.input.keyboard.on('keydown-ENTER', startCountdown);
+        }
+
+        function update() {
+            if (!pulsing && !isCountingDown) {
+                pulsing = true;
+                this.tweens.add({
+                    targets: [startText, enterText],
+                    scaleX: 1.1,
+                    scaleY: 1.1,
+                    yoyo: true,
+                    repeat: -1,
+                    ease: 'Sine.easeInOut',
+                    duration: 800
+                });
+            }
+
+            if (isCountingDown) {
+                countdownText.setText(`Starting in: ${countdown}`);
+            }
+        }
+
+        function startCountdown() {
+            if (!isCountingDown) {
+                isCountingDown = true;
+                startText.setVisible(false);
+                enterText.setVisible(false);
+                countdownText.setVisible(true);
+
+                const timer = setInterval(() => {
+                    countdown -= 1;
+                    if (countdown <= 0) {
+                        clearInterval(timer);
+                        startGame();
+                    }
+                }, 1000);
+            }
+        }
+
+        function startGame() {
+            console.log('Starting game!');
+            countdownText.setVisible(false);
+            // Placeholder for game start logic
+        }
+
+        return () => {
+            game.destroy(true);
+        };
+    }, []);
+
+    return <div id="phaser-game-container" className="phaser-container"></div>;
 }
 
 export default App;
